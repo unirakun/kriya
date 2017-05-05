27c4d28aa06176ee873658a8be72bb354b3b739a
Guillaume CRESPEL <guillaume.crespel@gmail.com>
2017-05-05 17:19:01 +0200

    style

--- src/input/input.jsx
+++ src/input/input.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Field } from 'redux-form'
 import classnames from 'classnames'
 import { onlyUpdateForPropTypes } from 'recompose'
-import Select from 'react-select'
+import Select from 'kriya-select'
 import styles from '../../src/input/input.styles.scss'
 
 const getComponent = (type) => {
@@ -18,60 +18,46 @@ const getComponent = (type) => {
 }
 
 const Input = ({
-                 className, style,
-                 type, name,
-                 label, placeholder,
-                 disabled,
-                 required,
-                 options,
-                 ...restProps
-               }) => {
+   className, style,
+   type, name, label,
+   placeholder, disabled,
+   required, options,
+   ...selectboxProps
+ }) => {
   const classes = classnames(
     styles.input,
     styles[`type-${type}`],
     className,
   )
 
-  // Field from redux-form
-  const Field = (
-    <Field
-      className={styles.field}
-      name={name}
-      component={getComponent(type)}
-      type={type}
-      placeholder={placeholder}
-      disabled={disabled}
-      required={required}
-    >
-      {type === 'select' ?
-        options.map(o => <option key={o.value} value={o.value}>{o.label}</option>)
-        : null
-      }
-    </Field>
-  )
-
-  // SelectBox from kriya-select
-  const SelectBox = (
-    <Select
-      simpleValue
-      name={name}
-      placeholder={placeholder}
-      disabled={disabled}
-      required={required}
-      options={options}
-      {...restProps}
-    />
-  )
-
   return (
     <div className={classes} style={style}>
       {label && <label htmlFor={name}>{label}{required && '*'}</label>}
       {
-        type === 'selectbox' ? SelectBox : Field
+        type === 'selectbox' ?
+          <Select
+            simpleValue
+            name={name}
+            placeholder={placeholder}
+            disabled={disabled}
+            required={required}
+            options={options}
+            {...selectboxProps}
+          /> :
+          <Field
+            className={styles.field}
+            name={name}
+            component={getComponent(type)}
+            type={type}
+            placeholder={placeholder}
+            disabled={disabled}
+            required={required}
+          >
+            {type === 'select' ? options.map(o => <option key={o.value} value={o.value}>{o.label}</option>) : null}
+          </Field>
       }
       {
-        type === 'select' &&
-        <i className={classnames(styles.arrow, 'mdv-expand_more')} />
+        type === 'select' && <i className={classnames(styles.arrow, 'mdv-expand_more')} />
       }
     </div>
   )
@@ -81,26 +67,29 @@ Input.propTypes = {
   className: PropTypes.string,
   style: PropTypes.object,
   placeholder: PropTypes.string,
+  value: PropTypes.string,
   name: PropTypes.string.isRequired,
   label: PropTypes.string,
   type: PropTypes.oneOf(['input', 'checkbox', 'textarea', 'radio', 'select', 'selectbox', 'number']),
   disabled: PropTypes.bool,
   required: PropTypes.bool,
   options: PropTypes.arrayOf(PropTypes.shape({
-    value: PropTypes.string,
-    label: PropTypes.string,
+    value: PropTypes.string, label: PropTypes.string,
   })),
+  onChange: PropTypes.func,
 }
 
 Input.defaultProps = {
   type: 'input',
   label: undefined,
+  value: undefined,
   disabled: false,
   required: false,
   placeholder: '',
   className: '',
   style: {},
   options: [],
+  onChange: undefined,
 }
 
 export default onlyUpdateForPropTypes(Input)
--- src/input/input.spec.js
+++ src/input/input.spec.js
@@ -6,12 +6,17 @@ import renderer from 'react-test-renderer'
 import { reduxForm } from 'redux-form'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import mockComponent from '__mocks__/component'
 import Input from './input'
+import InputContainer from './input.container'
 
-const store = createStore(() => ({}))
+jest.mock('kriya-select', () => mockComponent('kriya-select'))
+
 const Decorated = reduxForm({ form: 'testForm' })(Input)
+const DecoratedContainer = reduxForm({ form: 'formCont' })(InputContainer)
 
 const snap = ({ name = 'name', ...rest }) => {
+  const store = createStore(() => ({}))
   const component = renderer.create(
     <Provider store={store}>
       <Decorated {...rest} name={name} />
@@ -23,35 +28,89 @@ const snap = ({ name = 'name', ...rest }) => {
 }
 
 describe('common/Input', () => {
-  it('should have a default behaviour', () => snap({}))
-  it('should add custom className', () => snap({ className: 'custom' }))
-  it('should add custom style', () => snap({ style: { backgroundColor: 'red' } }))
+  describe('graphical (JSX)', () => {
+    it('should have a default behaviour', () => snap({}))
+    it('should add custom className', () => snap({ className: 'custom' }))
+    it('should add custom style', () => snap({ style: { backgroundColor: 'red' } }))
 
-  describe('labels', () => {
-    it('should print a label', () => snap({ label: 'my label' }))
-    it('should print a * on label when input is required', () => snap({ required: true, label: 'my label' }))
+    describe('labels', () => {
+      it('should print a label', () => snap({ label: 'my label' }))
+      it('should print a * on label when input is required', () => snap({
+        required: true,
+        label: 'my label',
+      }))
+    })
+
+    it('should print an icon when this is a select', () => snap({ type: 'select' }))
+    it('should print the placeholder', () => snap({ type: 'input', placeholder: 'My placeholder' }))
+    it('should be disabled', () => snap({ type: 'input', disabled: true }))
+
+    describe('types', () => {
+      it('should print a textarea', () => snap({ type: 'textarea' }))
+      it('should print a select', () => snap({ type: 'select' }))
+      it('should print a selectbox', () => snap({ type: 'selectbox' }))
+      it('should print an input (input)', () => snap({ type: 'input' }))
+      it('should print an input (checkbox)', () => snap({ type: 'checkbox' }))
+      it('should print an input (radio)', () => snap({ type: 'radio' }))
+      it('should print an input (number)', () => snap({ type: 'number' }))
+
+      const options = [
+        { value: 'o1', label: 'l1' },
+        { value: 'o4', label: 'l4' },
+        { value: 'o2', label: 'l2' },
+        { value: 'o3', label: 'l3' },
+      ]
+      it('should print options on select', () => snap({ type: 'select', options }))
+      it('should print options on selectbox', () => snap({ type: 'selectbox', options }))
+      it('shouldnt print options (not select)', () => snap({ type: 'input', options }))
+    })
   })
+  describe('container', () => {
+    const snapContainer = (state, props) => {
+      const store = createStore(() => (
+        {
+          form: {
+            form1: {
+              values: {
+                field1: '1',
+              },
+            },
+          },
+        }
+      ))
+      const component = renderer.create(
+        <Provider store={store}>
+          <DecoratedContainer {...props} />
+        </Provider>,
+      )
 
-  it('should print an icon when this is a select', () => snap({ type: 'select' }))
-  it('should print the placeholder', () => snap({ type: 'input', placeholder: 'My placeholder' }))
-  it('should be disabled', () => snap({ type: 'input', disabled: true }))
+      const tree = component.toJSON()
+      expect(tree).toMatchSnapshot()
+    }
 
-  describe('types', () => {
-    it('should print a textarea', () => snap({ type: 'textarea' }))
-    it('should print a select', () => snap({ type: 'select' }))
-    it('should print an input (input)', () => snap({ type: 'input' }))
-    it('should print an input (checkbox)', () => snap({ type: 'checkbox' }))
-    it('should print an input (radio)', () => snap({ type: 'radio' }))
-    it('should print an input (number)', () => snap({ type: 'number' }))
+    it('should pass value of redux form for select', () => snapContainer({}, {
+      form: 'form1',
+      name: 'field1',
+      options: [
+        { value: 'o1', label: 'l1' },
+        { value: 'o4', label: 'l4' },
+        { value: 'o2', label: 'l2' },
+        { value: 'o3', label: 'l3' },
+      ],
+      type: 'select',
+    }))
 
-    const options = [
-      { id: 'o1', label: 'l1' },
-      { id: 'o4', label: 'l4' },
-      { id: 'o2', label: 'l2' },
-      { id: 'o3', label: 'l3' },
-    ]
-    it('should print options on select', () => snap({ type: 'select', options }))
-    it('shouldnt print options (not select)', () => snap({ type: 'input', options }))
+    it('should pass value of redux form for selectbox', () => snapContainer({}, {
+      form: 'form1',
+      name: 'field1',
+      options: [
+        { value: 'o1', label: 'l1' },
+        { value: 'o4', label: 'l4' },
+        { value: 'o2', label: 'l2' },
+        { value: 'o3', label: 'l3' },
+      ],
+      type: 'selectbox',
+    }))
   })
 })
 
