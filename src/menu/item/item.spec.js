94f409cd951bb48da07e8e85c1bc3bebce125da4
Fabien JUIF <fabien.juif@zenika.com>
2017-05-16 10:42:10 +0200

    :sparkles: menu items handle multiple routes

--- src/menu/item/item.spec.js
+++ src/menu/item/item.spec.js
@@ -5,18 +5,22 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import mockComponent from '__mocks__/component'
 import Item from './item'
+import { mapStateToProps } from './item.container'
 
 jest.mock('../../button', () => mockComponent('button'))
 
 const component = (props) => {
   return renderer.create(
     <Item {...props} />,
-  )
+  ).toJSON()
 }
 
-const snap = (props) => {
-  const c = component(props)
-  const tree = c.toJSON()
+const connect = (props, initState) => {
+  return JSON.stringify(mapStateToProps(initState, props))
+}
+
+const snap = (props, initState) => {
+  const tree = initState ? connect(props, initState) : component(props)
   expect(tree).toMatchSnapshot()
 }
 
@@ -26,6 +30,29 @@ describe('common/Item', () => {
     it('should add custom style', () => snap({ style: { backgroundColor: 'red' } }))
     it('should have a default behaviour', () => snap({}))
     it('should take a name', () => snap({ name: 'a name' }))
+  })
+
+  describe('container', () => {
+    const initState = {
+      router: {
+        result: {
+          title: 'RESULT_1',
+          parent: {
+            title: 'RESULT_2',
+            parent: {
+              title: 'RESULT_3',
+            },
+          },
+        },
+      },
+    }
+
+    it('should inject selected for a simple route', () => snap({ title: 'RESULT_1' }, initState))
+    it('should inject selected for a nested route', () => snap({ title: 'RESULT_3' }, initState))
+    it('should inject selected for a simple route, with list', () => snap({ title: ['OUPS', 'RESULT_1'] }, initState))
+    it('should inject selected for a nested route, with list', () => snap({ title: ['OUPS', 'RESULT_3'] }, initState))
+    it('should not inject selected', () => snap({ title: 'OUPS' }, initState))
+    it('should not inject selected, with list', () => snap({ title: ['OUPS', 'OUPS2'] }, initState))
   })
 })
 
