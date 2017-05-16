c039e032643323bb9aea8a1120710b0a09ebc3d1
Fabien JUIF <fabien.juif@zenika.com>
2017-05-16 15:58:34 +0200

    :sparkles: menu items handle multiple routes (#34)

--- src/menu/item/item.container.js
+++ src/menu/item/item.container.js
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux'
 import Component from './item'
 
-const isRouteFound = (result, title) => result && result.title === title
+const isRouteFound = (result, title) => {
+  if (!result) return false
+  if (Array.isArray(title)) return !!title.find(t => result.title === t)
+  return result.title === title
+}
 
-const mapStateToProps = (state, { title }) => {
+export const mapStateToProps = (state, { title }) => {
   // We are looking top down the result tree
   let result = state.router.result
   let selected = isRouteFound(result, title)
