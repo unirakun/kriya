459951412e0f0f1c2a066d61933b9c13d39316be
Fabien JUIF <fabien.juif@zenika.com>
2017-06-15 16:57:18 +0200

    :truck: enable imports from kriya/{components,redux,hoc} (#56)

---
+++ src/redux.js
@@ -0,0 +1,4 @@
+export { default as cards } from './card/redux'
+export { default as navigation } from './navigation/redux'
+export { default as popover } from './popover/redux'
+export { default as tabs } from './tabs/redux'
--- src/tabs/redux/index.js
+++ src/tabs/redux/index.js
@@ -1,3 +1,10 @@
-export { default } from './tabs'
+import tabs from './tabs'
+import * as actions from './tabs.actions'
+import * as selectors from './tabs.selectors'
+
+Object.assign(tabs, actions, selectors)
+
 export * from './tabs.actions'
 export * from './tabs.selectors'
+export default tabs
+
