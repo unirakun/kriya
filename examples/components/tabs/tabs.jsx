ea52d7c3fc37a058d45bf33f7b937085fee6c8b1
guigail <guillaume.crespel@gmail.com>
2017-06-20 15:52:32 +0200

    review

--- examples/components/tabs/tabs.jsx
+++ examples/components/tabs/tabs.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import router from 'hoc-little-router'
 import classnames from 'classnames'
-import { Tabs as Component } from '../../../components'
+import { Tabs as KriyaTabs } from '../../../components'
 import styles from './tabs.styles.scss'
 
 const Tabs = () => {
   return (
     <div className={styles.tabs}>
-      <Component />
+      <KriyaTabs />
     </div>
   )
 }
