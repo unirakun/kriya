022905ac9828e7e5ec2a9e39a2e683280131965d
guigail <guillaume.crespel@gmail.com>
2017-06-20 15:48:50 +0200

    lint

--- examples/components/tabs/tabs.jsx
+++ examples/components/tabs/tabs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import router from 'hoc-little-router'
 import classnames from 'classnames'
-import { Tabs as Component} from '../../../components'
+import { Tabs as Component } from '../../../components'
 import styles from './tabs.styles.scss'
 
 const Tabs = () => {
