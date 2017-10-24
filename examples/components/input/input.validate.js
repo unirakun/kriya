2ea50a88f374e5a76c0a13734e39e2abcb8e2d4c
guigail <guillaume.crespel@gmail.com>
2017-10-24 10:03:10 +0200

    self review

--- examples/components/input/input.validate.js
+++ examples/components/input/input.validate.js
@@ -5,15 +5,15 @@ export default (values) => {
   }
 
   if (values.textArea && values.textArea.includes('l')) {
-    errors.textArea = 'input2 is required'
+    errors.textArea = 'l is for lol'
   }
 
   if (values.selectbox && values.selectbox.label.includes('R')) {
-    errors.selectbox = 'input2 is required'
+    errors.selectbox = 'Rick is bad !'
   }
-  console.log(values.select)
+
   if (values.select && values.select.includes('value1')) {
-    errors.select = 'input2 is required'
+    errors.select = 'Rick is bad !'
   }
 
   return errors
