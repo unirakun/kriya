c385ae8eecf31b7d2b67d3c87af462663bcc6c72
Delphine MILLET <dmillet@santeclair.fr>
2019-04-29 15:36:37 +0200

    upgrade webpack, babel, eslint, sass-lint

--- src/button/button.spec.js
+++ src/button/button.spec.js
@@ -11,6 +11,7 @@ import Button from './button'
 import ButtonContainer from './button.container'
 
 jest.mock('../icon', () => mockComponent('icon'))
+jest.mock('react-ink', () => mockComponent('Ink'))
 
 const snapshot = props => snap(Button)({ onClick: () => {}, ...props })
 
